106334500db708a72beddd6ef7aef7b87ca42892
Ashton Regensberg <aregensberg@gmail.com>
2020-05-28 17:05:36 -0600

    import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbedoimport ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed' gmerging to master properly.

---
+++ app/src/pages/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed'
+
+
+
+export const landing = () => {
+  <>
+    <div style={{ width: 660, height: 'auto' }}>
+      <ResponsiveEmbed aspectRatio="16by9">
+        <embed type="image/.jpg" src="/images/sky-space-dark-galaxy-2150.jpg" />
+      </ResponsiveEmbed>
+    </div>
+  </>
+}

89dea5ae4fdcd9afbae47825f1ac20505c8eb8c1
Ashton Regensberg <aregensberg@gmail.com>
2020-05-31 10:31:46 -0600

    added nav and landing basic elements

--- app/src/pages/Landing.js
+++ app/src/pages/Landing.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import Image from 'react-bootstrap/Image'
 import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed'
+import Card from 'react-bootstrap/Card'
+import CardColumns from 'react-bootstrap/CardColumns'
 
 
 export function landing() {
   return (
   <>
-    <div style={{ width: 660, height: 'auto' }}>
-      <ResponsiveEmbed aspectRatio="16by9">
-        <embed type="image/.jpg" src="/images/sky-space-dark-galaxy-2150.jpg" />
-      </ResponsiveEmbed>
-    </div>
+    <Image src="place holder" fluid />
+
+
+    <CardColumns>
+      <Card>
+        <Card.Body>
+          <Card.Text>
+            Lucas ipsum dolor sit amet gordin nadon fel bando skakoan koon moff ord darth kaleesh. Zam darth tusken raider ailyn tapani aayla muun sabé fett. Wirutid wat nautolan yuvernian zekk. Jettster wroonian firmus chagrian kast.
+            Padmé cathar ansion coruscant hutt nagai. Beru bibble b4-d4 jerjerrod durron. Jettster bib boba panaka. Maximilian yoda ponda woostoid togorian vebb ismaren luminara firrerreo. Senex plo kiffar wharl kiffar kit gricho croke yuzzem.
+            Ig-88 thul wirutid isolder desann greeata drach'nam subterrel.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </CardColumns>
   </>
   )
 }

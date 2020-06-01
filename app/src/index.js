87d228dd442655719e53f9dc296cff1d5948e619
Cody Ellison <ellison.cody24@gmail.com>
2020-06-01 16:45:24 -0600

    Merge branch 'sprint-2' of C:\Users\ellis\Desktop\bootcamp\Astronomy with conflicts.

--- app/src/index.js
+++ app/src/index.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css';
-import {Route, link, BrowserRouter as Router} from 'react-router-dom'
 import {FourOhFour} from "./pages/FourOhFour";
 import {Home} from "./pages/Home";
 import {Glossary} from './pages/Glossary'
 import {Landing} from './pages/Landing'
 import {NavBar} from './pages/NavBar'
-
-
-const Routing = (
-
-  <Router>
-    <div>
-    <Route exact path="/" component={Landing}/>
-    <Route exact path="/Glossary" component={Glossary}/>
-    <Route exact path="/Media" component={Media}/>
-    <Route exact path="/News" component={News}/>
-    <Route component={FourOhFour}/>
-    </div>
-  </Router>
-)
 
 
 ReactDOM.render(
@@ -31,6 +16,5 @@ ReactDOM.render(
       <Glossary />
     </div>
   </React.StrictMode>,
-  Routing,
   document.getElementById('root')
 );

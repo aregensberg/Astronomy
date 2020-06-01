55739bc401b0b7dcb21251a28a44ba545d103b8b
Ashton Regensberg <aregensberg@gmail.com>
2020-06-01 13:44:47 -0600

    adding index adds

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css';
-import {BrowserRouter} from "react-router-dom";
-import {Route, Switch} from "react-router";
 import {FourOhFour} from "./pages/FourOhFour";
 import {Home} from "./pages/Home";
 import {Glossary} from './pages/Glossary'

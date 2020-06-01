a9b8f441883d433ec26f710109e9f06dcac0d6b1
Ryan Mills <millselonius@gmail.com>
2020-06-01 14:53:21 -0600

    capitalization

--- app/src/index.js
+++ app/src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <div className="container">
       <NavBar />
-      <landing />
+      <Landing />
       <Glossary />
     </div>
   </React.StrictMode>,

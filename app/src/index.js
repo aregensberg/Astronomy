dff7b3b729691effadcb89dcf79e2870546a3ddd
Ryan Mills <millselonius@gmail.com>
2020-06-02 13:26:52 -0600

    restored routing

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css';
+import {Route, link, BrowserRouter as Router} from 'react-router-dom'
 import {FourOhFour} from "./pages/FourOhFour";
 import {Home} from "./pages/Home";
 import {Glossary} from './pages/Glossary'
 import {Landing} from './pages/Landing'
 import {NavBar} from './pages/NavBar'
 
+
+const Routing = (
+
+  <Router>
+    <div>
+      <Route exact path="/" component={Landing}/>
+      <Route exact path="/Glossary" component={Glossary}/>
+      <Route exact path="/Media" component={Media}/>
+      <Route exact path="/News" component={News}/>
+      <Route component={FourOhFour}/>
+    </div>
+  </Router>
+)
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,5 +31,6 @@ ReactDOM.render(
       <Glossary />
     </div>
   </React.StrictMode>,
+  Routing,
   document.getElementById('root')
 );

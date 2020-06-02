9d2633789f4fdd0886fb80c587dc35631804d5fa
Ryan Mills <millselonius@gmail.com>
2020-06-02 15:31:34 -0600

    divs

--- app/src/pages/News.js
+++ app/src/pages/News.js
@@ -3,7 +3,13 @@ import React from 'react'
 export function News() {
   return(
     <>
-      test
+      <div className="container text-center">
+        <div className="row">
+          <div className="col">
+            <h1>NEWS</h1>
+          </div>
+        </div>
+      </div>
     </>
   )
 }

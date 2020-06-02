2f4af8692796d4e77843baa0076487149e623395
Ashton Regensberg <aregensberg@gmail.com>
2020-06-02 15:31:30 -0600

    added proper route info

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.css';
-import {Route, link, BrowserRouter as Router} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Route, Switch } from 'react-router'
+
 import {FourOhFour} from "./pages/FourOhFour";
-import {Home} from "./pages/Home";
 import {Glossary} from './pages/Glossary'
 import {Landing} from './pages/Landing'
 import {NavBar} from './pages/NavBar'
@@ -11,28 +12,21 @@ import {Media} from './pages/Media'
 import {News} from './pages/News'
 
 
-const Routing = (
 
-  <Router>
-    <div>
+
+const Routing = () => (
+  <BrowserRouter>
+   <Switch>
       <Route exact path="/" component={Landing}/>
       <Route exact path="/Glossary" component={Glossary}/>
       <Route exact path="/Media" component={Media}/>
       <Route exact path="/News" component={News}/>
       <Route component={FourOhFour}/>
-    </div>
-  </Router>
+   </Switch>
+  </BrowserRouter>
 )
 
 ReactDOM.render(
-  <React.StrictMode>
-    <div className="container">
-      <NavBar />
-      <News />
-      <Landing />
-      <Glossary />
-    </div>
-  </React.StrictMode>,
-  Routing,
+  <Routing />,
   document.getElementById('root')
 );

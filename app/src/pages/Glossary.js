a1aea911a0394dbdf773196aaccd3c2053abb377
Ryan Mills <millselonius@gmail.com>
2020-06-03 14:06:58 -0600

    navbar

--- app/src/pages/Glossary.js
+++ app/src/pages/Glossary.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 export function Glossary() {
     return(
       <>
+        <Navbar bg="light" variant="light">
+          <Navbar.Brand href="#home">Astronomy</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#media">Media</Nav.Link>
+            <Nav.Link href="#glossary">Glossary</Nav.Link>
+            <Nav.Link href="#news">News</Nav.Link>
+          </Nav>
+        </Navbar>
         <div className="container text-center">
             <div className="row">
               <div className="col">

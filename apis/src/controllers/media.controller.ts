a6abade1d81a5f308b621bdd6eeab8fadaf744de
Cody Ellison <ellison.cody24@gmail.com>
2020-06-10 14:00:12 -0600

    added media controller

--- apis/src/controllers/media.controller.ts
+++ apis/src/controllers/media.controller.ts
@@ -1,8 +1,55 @@
+import {NextFunction, Request, Response} from "express";
+import {status} from "../../utils/interfaces/status";
+import {getAllMedia} from "../../utils/media/getAllMedia";
+import {getMediaByDescription} from "../../utils/media/getMediaByDescription";
+import {getMediaByImgId} from "../../utils/media/getMediaByImgId";
+
+
 export async function getAllMediaController(request: Request, response: Response, nextFunction: NextFunction) {
 
 	try {
 
 		const data = await getAllMedia()
+
+		console.log(data)
+
+		const status: Status = {status: 200, data, message: null}
+
+		return response.json(status)
+
+	} catch (error) {
+
+		console.log(error)
+
+	}
+
+}
+
+export async function getMediaByDescriptionController(request: Request, response: Response, nextFunction: NextFunction) {
+
+	try {
+
+		const data = await getMediaByDescription(request.body.description)
+
+		console.log(data)
+
+		const status: Status = {status: 200, data, message: null}
+
+		return response.json(status)
+
+	} catch (error) {
+
+		console.log(error)
+
+	}
+
+}
+
+export async function getMediaByImgIdController(request: Request, response: Response, nextFunction: NextFunction) {
+
+	try {
+
+		const data = await getMediaByImgId(request.body.id)
 
 		console.log(data)
 

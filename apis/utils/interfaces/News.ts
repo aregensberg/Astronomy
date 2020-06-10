d78fda2ffcc4b5edb503840e4edd41431290426c
Ryan Mills <millselonius@gmail.com>
2020-06-10 15:46:33 -0600

    news utils

--- apis/utils/interfaces/News.ts
+++ apis/utils/interfaces/News.ts
@@ -1,3 +1,8 @@
 export interface News {
-	
+    newsId: string,
+    newsImg: string,
+    newsDescription: string,
+    newsUrl: string,
+    newsTitle: string,
+    newsDate: string
 }
---
+++ apis/utils/news/getNewsByTitle.ts
@@ -0,0 +1,18 @@
+import {connect} from "../../src/database";
+
+
+/**
+ * Helper function that grabs all misquotes
+ **/
+export async function getNewsByTitle() {
+    try {
+
+        const mysqlConnection = await connect()
+        const mySqlQuery = "SELECT BIN_TO_UUID(newsId) AS newsId, newsImg, newsDescription, newsUrl, newsTitle, newsDate FROM News";
+        const [rows] = await mysqlConnection.execute(mySqlQuery)
+        return rows
+    } catch (error) {
+        console.log(error)
+        return undefined
+    }
+}

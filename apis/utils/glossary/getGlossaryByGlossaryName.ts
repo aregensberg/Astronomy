97b947bb7073457416ed48592e3334d57d8622ed
Ashton Regensberg <aregensberg@gmail.com>
2020-06-17 17:33:36 -0600

    added gloss validators, fixed route/controller after ts

--- apis/utils/glossary/getGlossaryByGlossaryName.ts
+++ apis/utils/glossary/getGlossaryByGlossaryName.ts
@@ -4,7 +4,7 @@ export async function getGlossaryByName(glossaryName:string) {
 
 	try {
 		const mysqlConnection = await connect()
-		const mySqlQuery = "SELECT BIN_TO_UUID(glossaryId) AS glossaryId, glossaryName, glossaryDefinition FROM glossary WHERE glossaryName= :glossaryName";
+		const mySqlQuery = "SELECT BIN_TO_UUID(glossaryName) AS glossaryName, BIN_TO_UUID(glossaryName) as glossaryId, glossaryName, glossaryDefinition FROM glossary WHERE glossaryName = UUID_TO_BIN(:glossaryName)";
 		const [rows] = await mysqlConnection.execute(mySqlQuery, {glossaryName: glossaryName})
 		return rows
 	} catch (error) {

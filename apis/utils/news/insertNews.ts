b1f1fe113d4a9f5f7f314d74a9c6acb9a7a69a40
Cody Ellison <ellison.cody24@gmail.com>
2020-06-19 12:08:28 -0600

    did some changes to insertNews.ts

--- apis/utils/news/insertNews.ts
+++ apis/utils/news/insertNews.ts
@@ -5,8 +5,9 @@ export async function insertNews(news: News) {
 
 	try {
 		const mySqlConnection = await connect()
+		console.log(news)
 
-		const mySqlQuery = "INSERT INTO news(newsId, newsImg, newsDescription, newsUrl, newsTitle, newsDate) VALUES(UUID_TO_BIN(UUID()),  :misquoteAttribution, :misquoteContent, :misquoteSubmitter)";
+		const mySqlQuery = "INSERT INTO news(newsId, newsImg, newsDescription, newsUrl, newsTitle, newsDate) VALUES(UUID_TO_BIN(UUID()),  :newsImg, :newsDescription, :newsUrl, :newsTitle, :newsDate)";
 		const [rows] = await mySqlConnection.execute(mySqlQuery, news)
 
 		return "News created successfully"
